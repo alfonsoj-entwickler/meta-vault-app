13031befa67767b76cd2f35a0cac92835e302b6b
alfonsoj-entwickler <alfonsoj.entwickler@gmail.com>
2026-05-28 22:07:20 +0200

    Add utility functions for downloading and supporting image types

---
+++ app/utils/fileImages.ts
@@ -0,0 +1,30 @@
+import { generateReadableFileName } from "./randonName";
+
+// Función auxiliar para descargar JPEGs reconstruidos
+export const descargarBlob = (base64Data: string, mimeType: string) => {
+  const byteString = atob(base64Data.split(",")[1]);
+  const ab = new ArrayBuffer(byteString.length);
+  const ia = new Uint8Array(ab);
+  for (let i = 0; i < byteString.length; i++)
+    ia[i] = byteString.charCodeAt(i);
+  const blob = new Blob([ab], { type: mimeType });
+  forzarDescargaBrowser(blob, "jpg");
+};
+
+// El disparador visual de la descarga en el navegador
+export const forzarDescargaBrowser = (blob: Blob, extension: string) => {
+  const url = URL.createObjectURL(blob);
+  const link = document.createElement("a");
+  link.href = url;
+  link.download = generateReadableFileName(extension);
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+  URL.revokeObjectURL(url);
+};
+
+export const supportedImage = (typeImg: File | null) => typeImg?.type === 'image/jpeg' ||
+  typeImg?.type === 'image/jpg' ||
+  typeImg?.type === 'image/png' ||
+  typeImg?.type === 'image/webp';
+
